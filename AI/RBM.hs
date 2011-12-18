e53d026d30590a688f7b4a9639e8f313b00d387e
chetant <chetant@gmail.com>
2011-12-18 00:05:08 -0500

    basic CD1 learning done. TODO: add CDn

--- AI/RBM.hs
+++ AI/RBM.hs
@@ -6,7 +6,7 @@ import Data.Random
 import Data.Random.Distribution.Bernoulli
 
 import qualified Data.Array.Repa as R
-import Data.Array.Repa(Z(..), All(..), (:.)(..), (*^), (+^), (-^), (/^))
+import Data.Array.Repa(transpose, Z(..), All(..), (:.)(..), (*^), (+^), (-^), (/^))
 import Data.Array.Repa.Algorithms.Matrix(multiplyMM)
 
 import Graphics.Rendering.Chart hiding (Vector)
@@ -15,31 +15,39 @@ import Graphics.Rendering.Chart.Plot.AreaSpots
 import Data.Colour.SRGB
 import Data.Accessor
 
+import Text.Printf
+
 type Vector = R.Array R.DIM1 Double
 type Matrix = R.Array R.DIM2 Double
 
 data RBM = RBM { 
-      rbm_visBias  :: !Vector -- i
-    , rbm_hidBias  :: !Vector -- j
-    , rbm_weights  :: !Matrix -- i * j
-    , rbm_weightsT :: !Matrix -- j * i
+      rbm_weights  :: !Matrix -- j * (i+1) the +1 is the hidden bias
+    , rbm_weightsT :: !Matrix -- i * (j+1) the +1 is the visible bias
     } deriving (Eq, Show)
 
 dim1 size = Z :. size
 dim2 rows cols = Z :. rows :. cols
 
+{-# INLINE prepend #-}
+prepend :: Vector -> Matrix -> Matrix
+prepend v m = transpose (vec2Mtx v R.++ (transpose m))
+
+{-# INLINE vec2Mtx #-}
+vec2Mtx :: Vector -> Matrix
+vec2Mtx v = R.extend (Z :. All :. (1 :: Int)) v
+
 fromLists :: Real a => ([a], [a], [[a]]) -> RBM
 fromLists (vs, hs, ws)
     | length ws /= lh = error $ "Malformed RBM!"
-    | otherwise = RBM { rbm_visBias = R.fromList (dim1 lv) (map realToFrac vs)
-                      , rbm_hidBias = R.fromList (dim1 lh) (map realToFrac hs)
-                      , rbm_weights = ws'
-                      , rbm_weightsT = transpose ws'
-                      }
+    | otherwise = RBM { rbm_weights  = h0s R.++ ws'
+                      , rbm_weightsT = v0s R.++ (transpose ws') }
     where lv = length vs
           lh = length hs
           ws' = R.fromList (dim2 lh lv) (concatMap (map realToFrac) ws)
+          v0s = R.fromList (dim2 lv 1) (map realToFrac vs)
+          h0s = R.fromList (dim2 lh 1) (map realToFrac hs)
 
+{-# INLINE sigmoid #-}
 sigmoid :: Floating a => a -> a
 sigmoid x = 1 / (1 + exp (-x))
 
@@ -49,50 +57,45 @@ vecFromList xs = R.fromList (dim1 $ length xs) (map realToFrac xs)
 mtxFromList :: Real a => (Int, Int) -> [[a]] -> Matrix
 mtxFromList (r, c) xs = R.fromList (dim2 r c) (concatMap (map realToFrac) xs)
 
+{-# INLINE mXv #-}
 mXv :: Matrix -> Vector -> Vector
 mXv m v = R.sum (vm *^ m)
-    where (_ :. lv) = R.extent v
-          (_ :. hm :. wm) = R.extent m
+    where (_ :. hm :. _) = R.extent m
           vm = R.extend (Z :. hm :. All) v
 
-transpose :: Matrix -> Matrix
-transpose m = R.backpermute extent' swap m
-    where swap (Z :. i :. j) = Z :. j :. i
-          extent' = swap (R.extent m)
+{-# INLINE oneVec #-}
+oneVec :: Vector
+oneVec = R.fromFunction (Z :. 1) (const 1)
 
+{-# INLINE dupVec #-}
 dupVec :: Int -> Vector -> Matrix
 dupVec n v = R.extend (Z :. n :. All) v
-    where (Z :. h) = R.extent v
 
 getHiddenPGivenV :: RBM -> Vector -> Vector
-getHiddenPGivenV rbm v = R.map sigmoid $ hbs +^ (ws `mXv` v)
-    where hbs = rbm_hidBias rbm
-          ws = rbm_weights rbm
+getHiddenPGivenV rbm v = R.map sigmoid $ (ws `mXv` (oneVec R.++ v))
+    where ws = rbm_weights rbm
 
 getVisiblePGivenH :: RBM -> Vector -> Vector
-getVisiblePGivenH rbm h = R.map sigmoid $ vbs +^ (ws `mXv` h)
-    where vbs = rbm_visBias rbm
-          ws = rbm_weightsT rbm
+getVisiblePGivenH rbm h = R.map sigmoid $ (ws `mXv` (oneVec R.++ h))
+    where ws = rbm_weightsT rbm
 
 getHiddenPGivenVs :: RBM -> Matrix -> Matrix
-getHiddenPGivenVs rbm vs = R.map sigmoid $ hbm +^ (R.sum (wm *^ vm))
-    where hbs = rbm_hidBias rbm
-          ws = rbm_weights rbm
-          (Z :. j :. i) = R.extent ws
+getHiddenPGivenVs rbm vs = R.map sigmoid $ R.sum (wm *^ vm)
+    where ws = rbm_weights rbm
+          (Z :. j :. _) = R.extent ws
           (Z :. n :. _) = R.extent vs
-          hbm = R.extend (Z :. n   :. All) hbs
+          oneV = R.fromFunction (Z :. n :. 1) (const 1)
           wm  = R.extend (Z :. n   :. All :. All) ws
-          vm  = R.extend (Z :. All :. j   :. All) vs
+          vm  = R.extend (Z :. All :. j   :. All) (oneV R.++ vs)
 
 getVisiblePGivenHs :: RBM -> Matrix -> Matrix
-getVisiblePGivenHs rbm hs = R.map sigmoid $ vbm +^ (R.sum (wm *^ hm))
-    where vbs = rbm_visBias rbm
-          ws = rbm_weightsT rbm
-          (Z :. i :. j) = R.extent ws
+getVisiblePGivenHs rbm hs = R.map sigmoid $ R.sum (wm *^ hm)
+    where ws = rbm_weightsT rbm
+          (Z :. i :. _) = R.extent ws
           (Z :. n :. _) = R.extent hs
-          vbm = R.extend (Z :. n   :. All) vbs
+          oneH = R.fromFunction (Z :. n :. 1) (const 1)
           wm  = R.extend (Z :. n   :. All :. All) ws
-          hm  = R.extend (Z :. All :. i   :. All) hs
+          hm  = R.extend (Z :. All :. i   :. All) (oneH R.++ hs)
 
 sampleP :: Vector -> IO Vector
 sampleP ps = R.fromList (R.extent ps) <$> sequence bs
@@ -114,18 +117,88 @@ samplePNs ps = R.fromList ext <$> sequence bs
 
 -- build expected value of <vh> given V(H) and `n` sampled H's(Vs)
 corr :: Vector -> Matrix -> Matrix
-corr v hs = R.sum $ vm *^ hm
+corr v hs = sh `prepend` (sv R.++ (R.sum $ vm *^ hm))
     where (_ :. n :. j) = R.extent hs
           (_ :. i) = R.extent v
+          hsT = transpose hs
           vm = R.extend (Z :. All :.   j :.   n) v
-          hm = R.extend (Z :. i   :. All :. All) $ transpose hs
+          hm = R.extend (Z :. i   :. All :. All) hsT
+          sv = vec2Mtx v -- i,1
+          sh = oneVec R.++ (R.sum hsT) -- <h> j + 1
 
 corrs :: Matrix -> Matrix -> Matrix
-corrs vs hs = R.sum $ vm *^ hm
+corrs vs hs = sh `prepend` (sv R.++ (R.sum $ vm *^ hm))
     where (Z :. n :. j) = R.extent hs
           (Z :. _ :. i) = R.extent vs
-          vm = R.extend (Z :. All :.   j :. All) $ transpose vs
-          hm = R.extend (Z :. i   :. All :. All) $ transpose hs
+          vsT = transpose vs
+          hsT = transpose hs
+          vm = R.extend (Z :. All :.   j :. All) vsT
+          hm = R.extend (Z :. i   :. All :. All) hsT
+          sv = vec2Mtx (R.sum vsT) -- i,1
+          sh = oneVec R.++ (R.sum hsT) -- <h> j + 1
+
+r = fromLists ([0.01, 0, 0.05], [0.035, (-0.013)], [[0.01, 0.06, 0.25], [-0.072, 0.048, 0.017]])
+v = vecFromList [1, 0, 1]
+e = mkWtConst r 0.07
+
+mkWtConst :: RBM -> Double -> Matrix
+mkWtConst r e = R.fromFunction (Z :. (h+1) :. w) (const e)
+    where (Z :. h  :. w) = R.extent (rbm_weightsT r)
+
+updateWeights :: RBM -> Matrix -> RBM
+updateWeights rbm delWt = rbm { rbm_weights  = (rbm_weights  rbm +^ dwt)
+                              , rbm_weightsT = (rbm_weightsT rbm +^ dwtT) }
+    where dwtT  = R.traverse delWt (\(Z :. h :. w) -> (Z :. (h-1) :. w)) (\f (Z :. i :. j) -> f (Z :. (i+1) :. j)) -- (i+1)x(j+1) -> (i)x(j+1)
+          dwt = R.traverse delWt (\(Z :. h :. w) -> (Z :. (w-1) :. h)) (\f (Z :. i :. j) -> f (Z :. j :. (i+1))) -- (i+1)x(j+1) -> (j)x(i+1)
+
+cdLearn :: Int -> Matrix -> RBM -> Vector -> IO (RBM, Double)
+cdLearn n epsilon rbm v = do
+  let numSamples = 10
+      ns = mkWtConst rbm (realToFrac numSamples)
+  hs <- samplePN (getHiddenPGivenV rbm v) numSamples
+  let posCor = corr v hs
+  vs' <- samplePNs (getVisiblePGivenHs rbm hs)
+  let negCor = corrs vs' hs
+  let delWt = epsilon *^ ((posCor -^ negCor) /^ ns)
+  let rbm' = updateWeights rbm delWt
+  -- TODO: Get reconstruction error squared
+  let origVs = dupVec numSamples v
+      err = (origVs -^ vs')
+      errSq = (R.sumAll (err *^ err))
+  -- showMat "hs" hs
+  -- showMat "posCor" posCor
+  -- showMat "vs'" vs'
+  -- showMat "negCor" negCor
+  -- showMat "epsilon" epsilon
+  -- showMat "delWt" delWt
+  -- print delWt
+  -- print rbm
+  return (rbm', errSq)
+
+learnLoop :: Int -> RBM -> Vector -> IO RBM
+learnLoop 0 rbm _ = return rbm
+learnLoop n rbm v = do
+  (rbm', err) <- cdLearn 1 e rbm v
+  putStrLn $ show n ++ "," ++ show err
+  -- putStrLn $ "N:" ++ show n ++ ", Error:" ++ show err
+  learnLoop (n-1) rbm' v
+
+reconstruct :: RBM -> Vector -> IO Vector
+reconstruct rbm v = do
+  let numSamples = 10
+  hs <- samplePN (getHiddenPGivenV rbm v) numSamples
+  vs <- samplePNs (getVisiblePGivenHs rbm hs)
+  let vs' = R.sum (transpose vs)
+  return vs'
+
+showVec :: Vector -> IO ()
+showVec v = putStrLn $ "  " ++ (foldr (\x s-> printf "%+3.2f" x ++ ",\t" ++ s) "" (R.toList v))
+
+showMat :: String -> Matrix -> IO ()
+showMat name m = do
+  let (Z :. h :. w) = R.extent m
+  putStrLn $ name ++ "(" ++ show h ++ ", " ++ show w ++ ") ="
+  mapM_ (\h -> showVec (R.slice m (Z :. h :. All))) [0..(h-1)]
 
 visualizeWeights :: RBM -> IO ()
 visualizeWeights rbm = do
@@ -152,56 +225,3 @@ visualizeWeights rbm = do
           (_ :. h :. w) = R.extent weights
           pts = [ (i `mod` w, h - (i `div` w), sz, round (255 * ((x - minWt)/wtRange))) | (i, x) <- zip [0..] (R.toList weights)]
 
-
--- r = fromLists ([0.01, 0, 0.05], [0.035, (-0.013)], [[0.01, 0.06, 0.25], [-0.072, 0.048, 0.017]])
-r = fromLists ([0, 0, 0], [0, 0], [[0.01, 0.06, 0.25], [-0.072, 0.048, 0.017]])
-v = vecFromList [1, 0, 1]
-e = mkWtConst r 0.07
-
-mkWtConst :: RBM -> Double -> Matrix
-mkWtConst r e = R.fromFunction (R.extent . rbm_weightsT $ r) (const e)
-
-updateWeights :: RBM -> Matrix -> RBM
-updateWeights rbm delWt = rbm { rbm_weights = (rbm_weights rbm +^ (R.transpose delWt))
-                              , rbm_weightsT = (rbm_weightsT rbm +^ delWt) }
-
-cdLearn :: Int -> Matrix -> RBM -> Vector -> IO (RBM, Double)
-cdLearn n epsilon rbm v = do
-  let numSamples = 10
-      ns = mkWtConst rbm 10
-  hs <- samplePN (getHiddenPGivenV rbm v) numSamples
-  let posCor = corr v hs
-  vs <- samplePNs (getVisiblePGivenHs rbm hs)
-  let negCor = corrs vs hs
-  let delWt = epsilon *^ ((posCor -^ negCor) /^ ns)
-  -- print posCor
-  -- print negCor
-  -- print ns
-  -- print delWt
-  let rbm' = updateWeights rbm delWt
-  -- TODO: Get reconstruction error squared
-  let origVs = dupVec numSamples v
-      err = (origVs -^ vs)
-      errSq = (R.sumAll (err *^ err))
-  -- print posCor
-  -- print negCor
-  -- print epsilon
-  -- print delWt
-  -- print rbm
-  return (rbm', errSq)
-
-learnLoop :: Int -> RBM -> Vector -> IO RBM
-learnLoop 0 rbm _ = return rbm
-learnLoop n rbm v = do
-  (rbm', err) <- cdLearn 1 e rbm v
-  putStrLn $ show n ++ "," ++ show err
-  -- putStrLn $ "N:" ++ show n ++ ", Error:" ++ show err
-  learnLoop (n-1) rbm' v
-
-reconstruct :: RBM -> Vector -> IO Vector
-reconstruct rbm v = do
-  let numSamples = 10
-  hs <- samplePN (getHiddenPGivenV rbm v) numSamples
-  vs <- samplePNs (getVisiblePGivenHs rbm hs)
-  let vs' = R.sum (transpose vs)
-  return vs'
